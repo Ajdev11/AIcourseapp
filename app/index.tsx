ddb3974fa560173a508467ec7a58823c46eb60fb
unknown <mosesogun11@gmail.com>
2025-02-05 10:56:35 +0100

    landing page for the courseApp

---
+++ app/index.tsx
@@ -0,0 +1,89 @@
+import { Text, View, Image, StyleSheet } from "react-native";
+import Colors from "./../constant/Colors";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: Colors.WHITE,
+      }}
+    >
+      <Image
+        source={require("../assets/images/landing.png")}
+        style={{
+          width: "100%",
+          height: 300,
+          marginTop: 70,
+        }}
+      />
+
+      <View
+        style={{
+          padding: 25,
+          backgroundColor: Colors.PRIMARY,
+          height: "100%",
+          borderTopLeftRadius: 35,
+          borderTopRightRadius: 35,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            textAlign: "center",
+            color: Colors.WHITE,
+          }}
+        >
+          Welcome to AICourseApp
+        </Text>
+        <Text
+          style={{
+            fontSize: 20,
+            color: Colors.WHITE,
+            textAlign: "center",
+            marginTop: 25,
+          }}
+        >
+          Learn a programming language, build a project, and get a job.
+          effortlessly with our intuitive platform.
+        </Text>
+
+        <View style={styles.button}>
+          <Text style={[styles.buttonText, { color: Colors.PRIMARY }]}>
+            Get Started
+          </Text>
+        </View>
+
+        <View
+          style={[
+            styles.button,
+            {
+              backgroundColor: Colors.PRIMARY,
+              borderWidth: 1,
+              borderColor: Colors.WHITE,
+            },
+          ]}
+        >
+          <Text style={[styles.buttonText, {color: Colors.WHITE}]}>Already have an account?</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+ button:{
+padding: 15,
+backgroundColor: Colors.WHITE,
+marginTop: 30,
+borderRadius: 10,
+},
+buttonText:{
+color: Colors.PRIMARY,
+fontWeight: "bold",
+textAlign: "center",
+fontSize: 18,
+}
+});
